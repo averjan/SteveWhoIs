9e38633b67eae950451e131bc8fdf550c09a4cfe
averjan <timophey.krut@gmail.com>
2021-08-11 00:20:29 +0300

    Add choosing bitmap image

--- ConsoleImageDrawer/Program.cs
+++ ConsoleImageDrawer/Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
+using System.Drawing;
+using System.IO;
 
 namespace ConsoleImageDrawer
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Print input bitmap:");
+            string sourceFilePath = Console.ReadLine();
+            Program.ConsolePrintImage(sourceFilePath);
+        }
+
+        private static void ConsolePrintImage(string imagePath)
+        {
+            Bitmap bitmap;
+            try
+            {
+                bitmap = new Bitmap(imagePath);
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File not found");
+                return;
+            }
+
+            for (int i = 0; i < bitmap.Width; i++)
+            {
+                for (int j = 0; j < bitmap.Height; j++)
+                {
+                    Program.ConsolePrintPixel(bitmap.GetPixel(i, j));
+                }
+            }
+        }
+
+        private static void ConsolePrintPixel(Color pixel)
+        {
+
         }
     }
 }
