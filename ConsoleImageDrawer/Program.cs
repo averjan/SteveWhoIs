91e710612ed2046a667777773f4ec2758544b08f
averjan <timophey.krut@gmail.com>
2021-08-11 00:03:14 +0300

    Initial commit

---
+++ ConsoleImageDrawer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleImageDrawer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
